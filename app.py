233f9b850d9f98d103f630181ccdc3cb11d2859e
Alice Agrawal <alice.agrawal30@gmail.com>
2022-06-01 19:05:29 -0700

    app completed

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.sidebar.caption('By [Alice Agrawal](https://www.linkedin.com/in/alice-agrawal
 articles_df = pd.read_csv('Data/articles.csv.zip')
 
 # Customer data for collabortive filtering
-df_customer = pd.read_csv('Data/df_customer.csv')
+df_customer = pd.read_csv('Data/df_customer.csv', index_col='customer_id')
 
 # Meta data for collabortive filtering
 transactions = pd.read_csv('Data/out.zip')
@@ -30,12 +30,14 @@ meta_data = pd.read_csv('Data/out_content.zip')
 
 # Import final collab model
 collab_model = pickle.load(open('Model/collaborative_model.sav', 'rb'))
+# st.write(articles_df['article_id'] == 893059004)
+
 
 # Def function using model to return recommendations - collaborative filtering
 def customer_article_recommend(customer,n_recs):
     
     have_bought = list(df_customer.loc[customer, 'article_id'])
-    not_bought = meta_articles.copy()
+    not_bought = articles_df.copy()
     not_bought.drop(have_bought, inplace=True)
     not_bought.reset_index(inplace=True)
     not_bought['est_purchase'] = not_bought['article_id'].apply(lambda x: collab_model.predict(customer, x).est)
@@ -48,13 +50,14 @@ def customer_article_recommend(customer,n_recs):
 
 # Second function for content based recommendations
 def article_recommend(article_input, n_recs2):
-    
-    article = articles_df.index[articles_df['article_id'] == article_input]
+    # st.write(article_input[0][0])
+    article = articles_df[articles_df['article_id'] == article_input].index
     y = np.array(meta_data.loc[article]).reshape(1, -1)
+    # st.write(article)
     cos_sim = cosine_similarity(meta_data, y)
     cos_sim = pd.DataFrame(data=cos_sim, index=meta_data.index)
     cos_sim.sort_values(by = 0, ascending = False, inplace=True)
-    results = cos_sim.index.values[1:n_recs+1]
+    results = cos_sim.index.values[1:n_recs2+1]
     results_df = articles_df.loc[results]
     results_df.reset_index(inplace=True)
     results_df.rename(columns={'prod_name':'Product Name', 'author':'Author',
@@ -75,11 +78,13 @@ page = st.sidebar.radio('Navigation', page_names)
 
 st.sidebar.caption('Please refer to my [Github](https://github.com/aliceagrawal/HM-Recommender-System-App) for reference to the code.')
 
+
+
 if page == 'Existing Customer':
     st.header("You chose the existing customer option.")
     customer_input = st.text_input("Please input your unique Customer ID.")
     n_recs = st.number_input("Please enter the number of article recommendations you would like.", max_value=20)
-    rec_button = st.button("For you...")
+    rec_button = st.button("Get some recommendations...")
     if rec_button:
         results = customer_article_recommend(customer_input, n_recs)
         st.table(results)
@@ -87,9 +92,10 @@ if page == 'Existing Customer':
 else:
     st.header("You chose the similar articles option.")
     article_input = st.number_input("Please enter a article ID.", max_value=959461001)
-    article = articles_df.index[articles_df['article_id'] == article_input]
+    # article = articles_df.index[articles_df['article_id'] == article_input]
     n_recs2 = st.number_input("Please enter the number of recommendations you would like.", max_value=20, key=2)
-    book_button = st.button("For you...", key=2)
+    book_button = st.button("Get some recommendations...", key=2)
     if book_button:
-        results2 = article_recommend(article, n_recs2)
+        results2 = article_recommend(article_input, n_recs2)
         st.table(results2)
+
